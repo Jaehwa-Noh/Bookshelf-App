68514591cce078ebb6ad7f155b06d3ee2806a892
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-13 20:27:39 +0900

    Create BookshelfAppBar composable.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfApp.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfApp.kt
@@ -1,9 +1,46 @@
 package com.example.bookshelfapp.ui
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import com.example.bookshelfapp.R
 
 @Composable
 fun BookshelfApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            BookshelfAppBar()
+        }
+    ) {
 
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookshelfAppBar(
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                stringResource(id = R.string.bookshelf),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.displaySmall
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier
+    )
 }
