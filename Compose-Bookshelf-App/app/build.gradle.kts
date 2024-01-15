4730b5bcc6f22ff0208f5b8008b3a04b28771c41
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-15 21:04:31 +0900

    Update gradle. - ktor-client-content-negotiation:2.3.7 - ktor-serialization-kotlinx-json:2.3.7

--- Compose-Bookshelf-App/app/build.gradle.kts
+++ Compose-Bookshelf-App/app/build.gradle.kts
@@ -58,6 +58,9 @@ android {
 
 dependencies {
 
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     implementation("io.ktor:ktor-client-core:2.3.7")
