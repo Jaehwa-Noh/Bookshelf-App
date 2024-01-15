73f0af89ae2a6e52e886df11e9d18eb10da7eb12
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-15 21:05:37 +0900

    Create getBook function, and apply HTTP get logic.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/network/GoogleBookApiService.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/network/GoogleBookApiService.kt
@@ -1,36 +1,51 @@
 package com.example.bookshelfapp.network
 
+import com.example.bookshelfapp.model.GoogleBookApiModel
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.ResponseException
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
 import io.ktor.http.URLProtocol
 import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface GoogleBookApiService {
-    suspend fun getBooks(searchTerms: String)
+    suspend fun getBooks(searchTerms: String): GoogleBookApiModel
 }
 
 @Singleton
 class GoogleBookApi @Inject constructor() : GoogleBookApiService {
-//    private val baseUrl = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+    //    private val baseUrl = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
     private val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json()
+        }
         defaultRequest {
             url {
                 protocol = URLProtocol.HTTPS
                 host = "https://www.googleapis.com/books/v1"
             }
         }
+        expectSuccess = true
     }
 
-    override suspend fun getBooks(searchTerms: String) {
-        val response = client.get {
-            url {
-                path("volumes")
-                parameters.append("q", searchTerms)
+    override suspend fun getBooks(searchTerms: String): GoogleBookApiModel {
+        val httpResponse = try {
+            client.get {
+                url {
+                    path("volumes")
+                    parameters.append("q", searchTerms)
+                }
             }
+        } catch (cause: ResponseException) {
+            throw cause
         }
+
+        return httpResponse.body<GoogleBookApiModel>()
     }
 }
