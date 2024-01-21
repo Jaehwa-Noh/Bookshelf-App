01733f54a65b7bd923227c6cad467523fdd20cb4
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-21 21:59:31 +0900

    Create GoogleBookRetrieveApiModel.

---
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/model/GoogleBookRetrieveApiModel.kt
@@ -0,0 +1,19 @@
+package com.example.bookshelfapp.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GoogleBookRetrieveApiModel(
+    val volumeInfo: RetrieveVolumeInfoModel
+)
+
+@Serializable
+data class RetrieveVolumeInfoModel(
+    val imageLinks: ThumbnailModel
+)
+
+@Serializable
+data class ThumbnailModel(
+    val thumbnail: String
+)
+
