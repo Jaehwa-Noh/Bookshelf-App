4c29896afba12182defd847d1a9efc6ca2ff5659
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-23 22:16:14 +0900

    Add Loading sub-object in BookShelfUiState.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfViewModel.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfViewModel.kt
@@ -6,6 +6,7 @@ class BookshelfViewModel {
 
 sealed interface BookShelfUiState {
     data class Success(val books: List<BooksUiModel>) : BookShelfUiState
+    data object Loading : BookShelfUiState
     data class Error(val error: String) : BookShelfUiState
 }
 
