77db4ff36a4a46186f672c2af6e3dde3dfd34f4e
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-23 21:18:09 +0900

    Create BooksDataSource.

---
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/data/BooksDataSource.kt
@@ -0,0 +1,24 @@
+package com.example.bookshelfapp.data
+
+import com.example.bookshelfapp.model.GoogleBookApiModel
+import com.example.bookshelfapp.network.GoogleBookApiService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface BooksDataSource {
+    suspend fun getBooks(searchTerms: String): GoogleBookApiModel
+}
+
+@Singleton
+class BooksNetworkDataSource @Inject constructor(
+    private val googleBookApiService: GoogleBookApiService,
+    private val ioDispatcher: CoroutineDispatcher
+) : BooksDataSource {
+    override suspend fun getBooks(searchTerms: String): GoogleBookApiModel {
+        return withContext(ioDispatcher) {
+            googleBookApiService.getBooks(searchTerms = searchTerms)
+        }
+    }
+}
