7b9c80a712742403aafd51db1edab40e20f06f96
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 12:47:32 +0900

    Add InstallIn Annotation.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/di/CoroutineDispatchersModule.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/di/CoroutineDispatchersModule.kt
@@ -2,10 +2,13 @@ package com.example.bookshelfapp.di
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 @Module
+@InstallIn(SingletonComponent::class)
 object CoroutineDispatchersModule {
 
     @DispatcherIO
