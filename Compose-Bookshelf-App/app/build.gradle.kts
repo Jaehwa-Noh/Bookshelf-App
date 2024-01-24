183dca19e31fa7ac13689a009b0173743ca211f8
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 12:55:04 +0900

    Update Gradle. - lifecycle-viewmodel-compose:2.7.0 - remove lifecycle-viewmodel-ktx

--- Compose-Bookshelf-App/app/build.gradle.kts
+++ Compose-Bookshelf-App/app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
     implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
