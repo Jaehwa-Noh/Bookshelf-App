fca539f9ed8221c19df4ff9f55cc4c0cff235cd6
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 19:30:49 +0900

    Update gradle. - coil-compose:2.5.0

--- Compose-Bookshelf-App/app/build.gradle.kts
+++ Compose-Bookshelf-App/app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
 
 dependencies {
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
     implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
