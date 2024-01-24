7abbe90b879c4a531464f5546d9fc11d62a02fab
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 20:10:22 +0900

    Apply SetHttpsUseCase.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/domain/GetBooksWithThumbnailsUseCase.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/domain/GetBooksWithThumbnailsUseCase.kt
@@ -10,18 +10,21 @@ import javax.inject.Inject
 class GetBooksWithThumbnailsUseCase @Inject constructor(
     private val booksRepository: BooksRepository,
     @DispatcherDefault private val defaultDispatcher: CoroutineDispatcher,
-    private val getBookIdUseCase: GetBookIdUseCase
+    private val getBookIdUseCase: GetBookIdUseCase,
+    private val setHttpsUseCase: SetHttpsUseCase
 ) {
     suspend operator fun invoke(searchTerms: String): List<BooksUiModel> {
         return withContext(defaultDispatcher) {
             val books = booksRepository.getBooks(searchTerms = searchTerms)
             val uiBooks = books.items.map { bookModel ->
                 val thumbnail =
-                    booksRepository
-                        .getThumbnails(bookId = getBookIdUseCase(bookModel.selfLink))
-                        .volumeInfo
-                        .imageLinks
-                        ?.thumbnail ?: ""
+                    setHttpsUseCase(
+                        url = booksRepository
+                            .getThumbnails(bookId = getBookIdUseCase(bookModel.selfLink))
+                            .volumeInfo
+                            .imageLinks
+                            ?.thumbnail ?: ""
+                    )
 
                 BooksUiModel(
                     id = bookModel.id,
