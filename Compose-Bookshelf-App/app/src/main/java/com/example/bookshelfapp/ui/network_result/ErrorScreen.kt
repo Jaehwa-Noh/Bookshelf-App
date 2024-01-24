93fd0812e7f36aaf5e062afb30d7204674251014
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 20:14:27 +0900

    Set size.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/network_result/ErrorScreen.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/network_result/ErrorScreen.kt
@@ -1,7 +1,9 @@
 package com.example.bookshelfapp.ui.network_result
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -9,8 +11,12 @@ fun ErrorScreen(
     modifier: Modifier = Modifier,
     error: String
 ) {
-    Text(
-        text = error,
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = error
+        )
+    }
 }
--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/network_result/LoadingScreen.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/network_result/LoadingScreen.kt
@@ -1,14 +1,19 @@
 package com.example.bookshelfapp.ui.network_result
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoadingScreen(
     modifier: Modifier = Modifier
 ) {
-    CircularProgressIndicator(
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
 }
