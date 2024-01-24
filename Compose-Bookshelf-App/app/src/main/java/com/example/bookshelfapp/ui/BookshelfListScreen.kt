44bdb800438e29fc1a4944dedc86e53ccf1281ab
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 19:23:38 +0900

    Set test string.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfListScreen.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfListScreen.kt
@@ -2,17 +2,22 @@ package com.example.bookshelfapp.ui
 
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun BookshelfListScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    books: List<BooksUiModel>
 ) {
     LazyVerticalGrid(
         modifier = modifier,
         columns = GridCells.Fixed(2)
     ) {
-
+        items(count = books.size, key = { books[it].id }) { index ->
+            val book = books[index]
+            Text(book.title)
+        }
     }
 }
