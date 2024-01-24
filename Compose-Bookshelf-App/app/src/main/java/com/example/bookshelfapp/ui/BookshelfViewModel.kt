24c5bb71984a8ce8733042c55598eff5afce957b
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 13:06:30 +0900

    Create getBooks() function.

--- Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfViewModel.kt
+++ Compose-Bookshelf-App/app/src/main/java/com/example/bookshelfapp/ui/BookshelfViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.bookshelfapp.ui
 
-class BookshelfViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bookshelfapp.domain.GetBooksWithThumbnailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BookshelfViewModel @Inject constructor(
+    private val getBooksWithThumbnailsUseCase: GetBooksWithThumbnailsUseCase
+) : ViewModel() {
+    private var _booksUiState: MutableStateFlow<BookShelfUiState> =
+        MutableStateFlow(BookShelfUiState.Loading)
+
+    val booksUiState: StateFlow<BookShelfUiState> = _booksUiState
+
+    init {
+        getBooks()
+    }
+
+    private fun getBooks() {
+        viewModelScope.launch {
+            _booksUiState.value = BookShelfUiState.Loading
+            _booksUiState.value = try {
+                BookShelfUiState.Success(
+                    getBooksWithThumbnailsUseCase("다이아몬드는 개똥밭에 굴러도 다이아몬드이다.")
+                )
+            } catch (e: Error) {
+                BookShelfUiState.Error(e.localizedMessage ?: "Error")
+            }
+        }
+    }
+
 }
 
 
