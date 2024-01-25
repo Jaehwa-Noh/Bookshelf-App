afe0c63e7181b96fc96a81fe8635a7a27677b19b
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-25 14:39:18 +0900

    Create BookshelfViewModelTest file.

---
+++ Compose-Bookshelf-App/app/src/test/java/com/example/bookshelfapp/BookshelfViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.example.bookshelfapp
+
+import com.example.bookshelfapp.data.BooksRepository
+import com.example.bookshelfapp.domain.GetBookIdUseCase
+import com.example.bookshelfapp.domain.GetBooksWithThumbnailsUseCase
+import com.example.bookshelfapp.domain.SetHttpsUseCase
+import com.example.bookshelfapp.fake.BooksFakeDataSource
+import com.example.bookshelfapp.fake.ThumbnailFakeDataSource
+import com.example.bookshelfapp.rules.TestDispatcherRule
+import com.example.bookshelfapp.ui.BookShelfUiState
+import com.example.bookshelfapp.ui.BooksUiModel
+import com.example.bookshelfapp.ui.BookshelfViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class BookshelfViewModelTest {
+    private lateinit var bookshelfViewModel: BookshelfViewModel
+    private lateinit var getBooksWithThumbnailsUseCase: GetBooksWithThumbnailsUseCase
+    private lateinit var booksRepository: BooksRepository
+    private lateinit var getBookIdUseCase: GetBookIdUseCase
+    private lateinit var setHttpsUseCase: SetHttpsUseCase
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Before
+    fun createBookshelfViewModel() {
+        getBookIdUseCase = GetBookIdUseCase(
+            defaultDispatcher = Dispatchers.Default
+        )
+
+        booksRepository = BooksRepository(
+            booksDataSource = BooksFakeDataSource(),
+            thumbnailDataSource = ThumbnailFakeDataSource()
+        )
+
+        setHttpsUseCase = SetHttpsUseCase(
+            defaultDispatcher = Dispatchers.Default
+        )
+
+        getBooksWithThumbnailsUseCase = GetBooksWithThumbnailsUseCase(
+            booksRepository = booksRepository,
+            defaultDispatcher = Dispatchers.Default,
+            getBookIdUseCase = getBookIdUseCase,
+            setHttpsUseCase = setHttpsUseCase,
+        )
+
+        runTest {
+            bookshelfViewModel = BookshelfViewModel(
+                getBooksWithThumbnailsUseCase = getBooksWithThumbnailsUseCase
+            )
+        }
+    }
+
+    @Test
+    fun bookshelfViewModelTest_LoadingSuccess_BookUiStateIsSuccessSuccess() {
+        assertTrue(bookshelfViewModel.booksUiState.value is BookShelfUiState.Success)
+    }
+
+    @Test
+    fun bookshelfViewModelTest_LoadingSuccess_BookUiStateValueCheckSuccess() {
+        assertTrue(bookshelfViewModel.booksUiState.value is BookShelfUiState.Success)
+
+        val books = (bookshelfViewModel.booksUiState.value as BookShelfUiState.Success).books
+        assertTrue(books.size == 2)
+
+        assertEquals(
+            books[0],
+            BooksUiModel(id = "id1", title = "title1", url = "thumbnail")
+        )
+    }
+}
