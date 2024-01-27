0485aa417ea31ef44934f12d738a008b2835287a
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-27 23:45:39 +0900

    Create BooksDataSource.

---
+++ SwiftUI-Bookshelf-App/Bookshelf app/Data/BooksDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  BooksDataSource.swift
+//  Bookshelf app
+//
+//  Created by Jaehwa Noh on 1/27/24.
+//
+import Factory
+
+protocol BooksDataSource {
+    func getBooks(searchTerms: String) async throws -> GoogleBookApiModel
+}
+
+struct BooksNetworkDataSource: BooksDataSource {
+    @Injected(\.googleBookApiService) var googleBookApiService
+    @Injected(\.userInitiated) var ioTask
+    
+    func getBooks(searchTerms: String) async throws -> GoogleBookApiModel {
+        do {
+            return try await Task.detached(priority: ioTask) {
+                do {
+                    return try await googleBookApiService.getBooks(searchTerm: searchTerms)
+                }
+            }.result.get()
+        } catch {
+            throw error
+        }
+    }
+    
+    
+}
