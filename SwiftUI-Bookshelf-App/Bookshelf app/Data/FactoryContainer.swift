603009722142162fcdf48bfc8469d3196fdbedd9
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-27 23:38:09 +0900

    Create Container extension.

---
+++ SwiftUI-Bookshelf-App/Bookshelf app/Data/FactoryContainer.swift
@@ -0,0 +1,25 @@
+//
+//  FactoryContainer.swift
+//  Bookshelf app
+//
+//  Created by Jaehwa Noh on 1/27/24.
+//
+
+import Factory
+
+extension Container {
+    var bookshelfViewModel: Factory<BookshelfViewModel> {
+        self {
+            BookshelfViewModel()
+        }
+    }
+    
+    var googleBookApiService: Factory<GoogleBookApiService> {
+        self {
+            GoogleBookApi()
+        }
+        .singleton
+    }
+}
+
+
