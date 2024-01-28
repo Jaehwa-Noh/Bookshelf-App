2c2ce360a52d85da89515ba98e00445261c67058
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-28 11:46:46 +0900

    Create BookThumbnailDataSource and BookThumbnailNetworkDataSourcce.

---
+++ SwiftUI-Bookshelf-App/Bookshelf app/Data/BookThumbnailDataSource.swift
@@ -0,0 +1,29 @@
+//
+//  BookThumbnailDataSource.swift
+//  Bookshelf app
+//
+//  Created by Jaehwa Noh on 1/28/24.
+//
+
+import Factory
+
+protocol BookThumbnailDataSource {
+    func getThumbnails(bookId: String) async throws -> GoogleBookRetrieveApiModel
+}
+
+struct BookThumbnailNetworkDataSource: BookThumbnailDataSource {
+    @Injected(\.googleBookApiService) var googleBookApiService
+    @Injected(\.userInitiated) var ioTask
+    
+    func getThumbnails(bookId: String) async throws -> GoogleBookRetrieveApiModel {
+        do {
+            return try await Task.detached(priority: ioTask) {
+                do {
+                    return try await  googleBookApiService.getThumbnail(bookId: bookId)
+                }
+            }.result.get()
+        } catch {
+            throw error
+        }
+    }
+}
