9e97ac57e7c5bf9779a19881187455bdf57012b7
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-29 12:57:30 +0900

    Create switch statement, and Inject bookshelfViewModel.

--- SwiftUI-Bookshelf-App/Bookshelf app/UI/BookshelfApp.swift
+++ SwiftUI-Bookshelf-App/Bookshelf app/UI/BookshelfApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Factory
 
 struct BookshelfApp: View {
+    @InjectedObject(\.bookshelfViewModel) var bookshelfViewModel
+    
     var body: some View {
         NavigationStack {
-            BookshelfListView()
-            .navigationTitle("Book shelf")
+            switch bookshelfViewModel.bookShelfUiState {
+            case .success(let books):
+                BookshelfListView(books: books)
+                    .navigationTitle("Book shelf")
+            case .loading:
+                LoadingView()
+                    .navigationTitle("Book shelf")
+            case .error(let error):
+                ErrorView(errorMessage: error,
+                          onRetryButtonClick: bookshelfViewModel.getBooks)
+                .navigationTitle("Book shelf")
+            }
         }
     }
 }
