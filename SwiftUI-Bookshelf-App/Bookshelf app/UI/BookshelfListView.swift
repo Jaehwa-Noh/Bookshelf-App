f75e2b36f12aa8fa3ce811a228ec8746220c2f03
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-29 13:29:53 +0900

    Create BookshelfListView.

---
+++ SwiftUI-Bookshelf-App/Bookshelf app/UI/BookshelfListView.swift
@@ -0,0 +1,44 @@
+//
+//  BookshelfListView.swift
+//  Bookshelf app
+//
+//  Created by Jaehwa Noh on 1/27/24.
+//
+
+import SwiftUI
+import Factory
+
+struct BookshelfListView: View {
+    let books: [BookshelfUiModel]
+    let columns = [GridItem(.flexible(), spacing: 0, alignment: .topLeading), GridItem(.flexible(), spacing: 0, alignment: .topLeading)]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(books, id: \.id) { book in
+                    if book.url.isEmpty {
+                        VStack {
+                            Text(book.title)
+                                .multilineTextAlignment(.leading)
+                            Spacer()
+                        }
+                        .padding([.horizontal], 4)
+                        .frame(height: 250)
+                    } else {
+                        AsyncImage(url: URL(string: book.url)) {
+                            image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(height: 250)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    BookshelfListView(books: [])
+}
